470bd9b2836b421c8e2cce5b66c5e508b18e70f4
Shubh Gupta <shubh.gupta.0797@gmail.com>
2021-12-26 00:06:25 +0530

    Environment to production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
+const HOSTNAME: string = "reqres.in";
+const PORT_NUMBER: number=0;
+const API: string = "/api/";
+
 export const environment = {
-  production: true
+  production: true,
+  
+  getListUsers: "https://" + HOSTNAME + API + "users?page=",
+  getSingleUser: "https://" + HOSTNAME + API + "users/",
+  postLogin: "https://" + HOSTNAME + API + "login",
+  postRegister: "https://" + HOSTNAME + API + "register",
 };
